9a81a62a3df635d7b7836e11c7be8bf73696e008
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-14 17:14:36 -0300

    Configuração do idioma e time zone

--- escolar/settings.py
+++ escolar/settings.py
@@ -72,9 +72,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
