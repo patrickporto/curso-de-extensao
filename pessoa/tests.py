1814182e5c44858d20ffc5befd639a9a50348339
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-14 19:17:12 -0300

    Atualização dos testes de pessoa

--- pessoa/tests.py
+++ pessoa/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from datetime import date
-from pessoa.models import Pessoa
+from pessoa.models import Pessoa, Contato, Aluno
 
 class PessoaTest(TestCase):
 
@@ -10,6 +10,43 @@ class PessoaTest(TestCase):
         pessoa.sobrenome = 'da Silva'
         pessoa.data_nascimento = date(1994, 1, 19)
         pessoa.cpf = '111.111.111-11'
-        pessoa.telefone = '(00)0000-0000'
-        pessoa.email = 'fulano@email.com'
         pessoa.save()
+        contato_1 = Contato.objects.create(
+            tipo=Contato.TELEFONE,
+            descricao='(00)0000-0000',
+            pessoa=pessoa
+        )
+        contato_2 = Contato.objects.create(
+            tipo=Contato.EMAIL,
+            descricao='fulano@email.com',
+            pessoa=pessoa
+        )
+
+        self.assertTrue(pessoa)
+        self.assertTrue(contato_1)
+        self.assertTrue(contato_2)
+
+
+class AlunoTest(TestCase):
+
+    def test_create_aluno(self):
+        aluno = Aluno()
+        aluno.nome = 'Fulano'
+        aluno.sobrenome = 'da Silva'
+        aluno.data_nascimento = date(1994, 1, 19)
+        aluno.cpf = '111.111.111-11'
+        aluno.save()
+        contato_1 = Contato.objects.create(
+            tipo=Contato.TELEFONE,
+            descricao='(00)0000-0000',
+            pessoa=aluno
+        )
+        contato_2 = Contato.objects.create(
+            tipo=Contato.EMAIL,
+            descricao='fulano@email.com',
+            pessoa=aluno
+        )
+
+        self.assertTrue(aluno)
+        self.assertTrue(contato_1)
+        self.assertTrue(contato_2)
