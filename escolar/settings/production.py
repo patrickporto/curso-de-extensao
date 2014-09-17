d8087b7a21a729d5e17e6effcfc8faa7724be050
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-17 00:25:22 -0300

    Configuração do ambiente de produção.

--- escolar/settings/production.py
+++ escolar/settings/production.py
@@ -1,10 +1,12 @@
 from escolar.settings import *
 
+ALLOWED_HOSTS = ('104.131.39.168',)
+
 DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'escolar',
         'USER': 'root',
         'PASSWORD': 'e0a2201f4681784d8c2725fea98a400a40396ffa4cdbb58eb0bc7514190b6a2dec7d55d9a9c525b8b01c04d2af42713c3787bf1b40ca24a3ffd48c75ba9a97d3',
