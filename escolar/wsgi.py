a555b18cb958f92ef1be94896ecbd7ba285227d7
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-17 00:26:55 -0300

    Configuração do MySQL no Python 3

--- escolar/wsgi.py
+++ escolar/wsgi.py
@@ -10,5 +10,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "escolar.settings")
 
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
