1df07331902c962f4e547dd393e288acdfc25a35
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-17 00:26:31 -0300

    Configuração do gunicorn

--- escolar/gunicorn.py
+++ escolar/gunicorn.py
@@ -1,7 +1,10 @@
 import multiprocessing
 
 reload = True
-bind = "127.0.0.1:9000"
+bind = "0.0.0.0:9000"
 workers = multiprocessing.cpu_count() * 2 + 1
 chdir = "/opt/app"
 pythonpath = "/opt/env/lib/python3.4/site-packages"
+raw_env = [
+    'DJANGO_SETTINGS_MODULE=escolar.settings.production'
+]
