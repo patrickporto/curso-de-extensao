72480371788e79605aac33573450d709a8a66a90
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-19 02:02:32 -0300

    Autenticação pelo portal

--- portal/urls.py
+++ portal/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('portal.views',
 
     url(r'^$', 'home', name='home'),
     url(r'^contato/', 'contato', name='contato'),
+    url(r'^access/', 'access', name='access'),
+    url(r'^logout/', 'user_logout', name='logout'),
 )
--- portal/views.py
+++ portal/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
 
 
 def home(request):
-	return render(request, 'base.html')
+    return render(request, 'base.html')
 
 
 def contato(request):
-	return render(request, 'base.html')
+    return render(request, 'base.html')
+
+
+def access(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        messages.add_message(request, messages.SUCCESS, 'Usuário autenticado com sucesso!')
+    else:
+        messages.add_message(request, messages.ERROR, 'Usuário ou senha inválida!')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
