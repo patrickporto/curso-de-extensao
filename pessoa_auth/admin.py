96230dbacd088af40b274748bb0f01ba1a5f76c6
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-19 16:50:32 -0300

    Correção do formulário de criação do usuário

--- pessoa_auth/admin.py
+++ pessoa_auth/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.forms import AdminPasswordChangeForm
+from django.contrib.auth.admin import UserAdmin
 from pessoa_auth.models import CustomUser
 from pessoa_auth.forms import UserChangeForm, UserCreationForm
 from pessoa.admin import PessoaAdmin
@@ -8,20 +9,24 @@ from pessoa.admin import PessoaAdmin
 # Register your models here.
 
 @admin.register(CustomUser)
-class CustomUserAdmin(PessoaAdmin):
+class CustomUserAdmin(PessoaAdmin, UserAdmin):
 	form = UserChangeForm
 	add_form = UserCreationForm
 
 	list_display = ('cpf', 'nome_completo', 'data_nascimento', 'is_superuser',)
-	list_filter = ('is_superuser',)
-
-	add_fieldsets = (
-		(None, {
-			'classes': ('wide',),
+	list_filter = ('is_superuser', 'is_active',)
+	fieldsets = (
+		('Informações Pessoais', {
+			'fields': ('nome', 'sobrenome','cpf', 'data_nascimento',),
+		}),
+		('Permissões', {
+			'fields': ('is_superuser', 'is_active',),
 		})
 	)
+	add_fieldsets = fieldsets
 
 	search_fields = ('cpf', 'nome', 'sobrenome',)
 	ordering = ('nome', 'sobrenome',)
+	filter_horizontal = ()
 
 admin.site.unregister(Group)
