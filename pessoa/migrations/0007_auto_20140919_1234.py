b82a04ddffefddcb7dba67d0a88f1b95cdaafadd
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-19 16:01:23 -0300

    Refatoração de migração

--- pessoa/migrations/0007_auto_20140919_1234.py
+++ pessoa/migrations/0007_auto_20140919_1234.py
@@ -12,21 +12,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='funcionario',
-            name='pessoa_ptr',
-        ),
         migrations.DeleteModel(
             name='Funcionario',
-        ),
-        migrations.AlterField(
-            model_name='pessoa',
-            name='data_atualizacao',
-            field=models.DateTimeField(auto_now=True, default=datetime.datetime(2014, 9, 19, 12, 34, 59, 626122), verbose_name='Data de atualização'),
-        ),
-        migrations.AlterField(
-            model_name='pessoa',
-            name='data_criacao',
-            field=models.DateTimeField(default=datetime.datetime(2014, 9, 19, 12, 34, 59, 626069), verbose_name='Data de criação', auto_now_add=True),
         ),
     ]
