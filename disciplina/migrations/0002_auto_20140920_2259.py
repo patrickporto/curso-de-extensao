776a87737ba686cedd097e9a2aa1a66b3202629c
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-20 23:28:24 -0300

    Migração do banco para criar o período

---
+++ disciplina/migrations/0002_auto_20140920_2259.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('disciplina', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Periodo',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('nome', models.CharField(max_length=255)),
+                ('data_inicio', models.DateField(verbose_name='Data de início', default=django.utils.timezone.now)),
+                ('data_termino', models.DateField(verbose_name='Data de término', default=django.utils.timezone.now)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.RemoveField(
+            model_name='disciplina',
+            name='data_inicio',
+        ),
+        migrations.RemoveField(
+            model_name='disciplina',
+            name='data_termino',
+        ),
+        migrations.AddField(
+            model_name='disciplina',
+            name='periodo',
+            field=models.ManyToManyField(to='disciplina.Periodo'),
+            preserve_default=True,
+        ),
+    ]
