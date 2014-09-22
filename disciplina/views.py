752e4100114eafbfcca7f787dc785a6dae093fdc
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-22 13:06:02 -0300

    Atualização da página de disciplinas

--- disciplina/views.py
+++ disciplina/views.py
@@ -6,15 +6,18 @@ from disciplina.models import Avaliacao, Periodo
 
 @user_passes_test(lambda u: u.tipo == u.ALUNO)
 def disciplinas(request):
-    periodo_id = request.GET.get('periodo')
+    periodo_id = int(request.GET.get('periodo', 0))
     context = {}
     avaliacoes = Avaliacao.objects.filter(aluno=request.user)
-    context['periodos'] = Periodo.objects.filter(pk__in=avaliacoes.values('disciplina__periodo').distinct())
+    periodos = Periodo.objects.filter(pk__in=avaliacoes.values('disciplina__periodo').distinct())
     if periodo_id:
         avaliacoes = avaliacoes.filter(disciplina__periodo__id=periodo_id)
-        if avaliacoes.count() == 0:
-            messages.add_message(request, messages.ERROR, 'O período informado não existe')
+    else:
+        avaliacoes = avaliacoes.filter(disciplina__periodo=periodos.last())
+    if avaliacoes.count() == 0 and periodo_id:
+        messages.add_message(request, messages.ERROR, 'O período informado não existe')
 
     context['periodo_id'] = periodo_id
     context['avaliacoes'] = avaliacoes
+    context['periodos'] = periodos
     return render(request, 'disciplinas.html', context)
