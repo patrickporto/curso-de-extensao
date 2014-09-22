da943cc95a5f63c207fee53bc5f717ed7e6afa05
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-22 02:42:32 -0300

    Correção de documentos pendentes

--- pessoa/views.py
+++ pessoa/views.py
@@ -9,14 +9,15 @@ from django.contrib import messages
 def documentos(request):
     context = {}
     documentos_pendentes = []
-    obj = DocumentosPendentes.objects.get(aluno=request.user)
-    for documento in obj.documentos[1:-1].split(','):
-        if not documento:
-            continue
-        documentos_pendentes.append(get_documento_display(int(documento.strip()[1:-1])))
-    if obj.outros:
-        documentos_pendentes += obj.outros.split(',')
-    context['documentos'] = documentos_pendentes
+    obj = DocumentosPendentes.objects.filter(aluno=request.user).first()
+    if obj:
+        for documento in obj.documentos[1:-1].split(','):
+            if not documento:
+                continue
+            documentos_pendentes.append(get_documento_display(int(documento.strip()[1:-1])))
+        if obj.outros:
+            documentos_pendentes += obj.outros.split(',')
+        context['documentos'] = documentos_pendentes
     if len(documentos_pendentes) == 0:
         messages.add_message(request, messages.INFO, 'Você não possui documentos pendentes')
     return render(request, 'documentos.html', context)
