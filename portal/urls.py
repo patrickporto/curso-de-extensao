585f6369246c489c9466175a2ce82219d5eb09eb
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-22 00:13:11 -0300

    Remoção de comentário

--- portal/urls.py
+++ portal/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('portal.views',
-    # Examples:
-    # url(r'^$', 'escolar.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', 'home', name='home'),
     url(r'^contato/$', 'contato', name='contato'),
     url(r'^access/$', 'access', name='access'),
