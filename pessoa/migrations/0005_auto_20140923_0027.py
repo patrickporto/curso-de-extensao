8f280bbfd46b7fb105cbe7e33c4801314f587904
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-23 00:28:55 -0300

    Migração de alteração do campo outros de documentos pendentes

---
+++ pessoa/migrations/0005_auto_20140923_0027.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pessoa', '0004_auto_20140922_0028'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='documentospendentes',
+            name='outros',
+            field=models.CharField(blank=True, default='', verbose_name='Outros', max_length=255, help_text='Por favor, use vírgula (,) como separador para definir multiplos documentos'),
+        ),
+    ]
