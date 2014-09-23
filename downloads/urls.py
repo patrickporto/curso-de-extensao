c5e137f69db24d39f87282728c18c1af21efdf50
Igor Canedo <igor.canedo@corp.globo.com>
2014-09-23 03:02:11 -0300

    Correção da regra da url de download

--- downloads/urls.py
+++ downloads/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('downloads.views',
     url(r'^$', 'arquivos', name="arquivos"),
-    url(r'^download/(?P<slug>\w*)', 'download', name="download"),
+    url(r'^download/(?P<slug>[\w_-]*)/$', 'download', name="download"),
 )
