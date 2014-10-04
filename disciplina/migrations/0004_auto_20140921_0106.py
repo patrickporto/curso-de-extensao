8868f2bd0fac3fec1bafa49f18e17488a5b5abde
Igor Canedo <igor.canedo@corp.globo.com>
2014-10-04 18:43:14 -0300

    Correção na migração

--- disciplina/migrations/0004_auto_20140921_0106.py
+++ disciplina/migrations/0004_auto_20140921_0106.py
@@ -5,39 +5,22 @@ from django.db import models, migrations
 from django.conf import settings
 
 
-def migrate_professores(apps, schema_editor):
-    Disciplina = apps.get_model('disciplina', 'Disciplina')
-    for disciplina in Disciplina.objects.all():
-        disciplina.professor_temp = disciplina.professor.first()
-        disciplina.save()
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('disciplina', '0003_auto_20140921_0054'),
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='disciplina',
-            name='professor_temp',
-            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='professor', null=True),
-            preserve_default=True,
-        ),
-        migrations.RunPython(migrate_professores),
-        migrations.AlterField(
-            model_name='disciplina',
-            name='professor_temp',
-            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='professor', null=False),
-        ),
         migrations.RemoveField(
             model_name='disciplina',
             name='professor',
         ),
-        migrations.RenameField(
+        migrations.AddField(
             model_name='disciplina',
-            old_name='professor_temp',
-            new_name='professor',
+            name='professor',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='professores', null=True),
+            preserve_default=True,
         ),
     ]
