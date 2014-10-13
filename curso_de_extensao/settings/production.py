6ee998eee9dabfea98d55d16c2d2e772fd922b0f
Igor Canedo <igor.canedo@corp.globo.com>
2014-10-12 22:38:26 -0300

    Correção das credenciais do banco

--- curso_de_extensao/settings/production.py
+++ curso_de_extensao/settings/production.py
@@ -9,8 +9,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cursodeextensao',
-        'USER': 'root',
-        'PASSWORD': 'e0a2201f4681784d8c2725fea98a400a40396ffa4cdbb58eb0bc7514190b6a2dec7d55d9a9c525b8b01c04d2af42713c3787bf1b40ca24a3ffd48c75ba9a97d3',
+        'USER': 'escola',
+        'PASSWORD': '#!Q@W#E$R',
     }
 }
 
