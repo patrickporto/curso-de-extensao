cc2bded52610a3f22ae0f35970ec42c8d1999d9d
Igor Canedo <igor.canedo@corp.globo.com>
2014-10-12 22:21:00 -0300

    Correção do nome do banco de produção

--- curso_de_extensao/settings/production.py
+++ curso_de_extensao/settings/production.py
@@ -8,7 +8,7 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'escolar',
+        'NAME': 'cursodeextensao',
         'USER': 'root',
         'PASSWORD': 'e0a2201f4681784d8c2725fea98a400a40396ffa4cdbb58eb0bc7514190b6a2dec7d55d9a9c525b8b01c04d2af42713c3787bf1b40ca24a3ffd48c75ba9a97d3',
     }
