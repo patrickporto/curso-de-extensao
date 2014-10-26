1bc5170620e2a7ed80caec46100bdec42809ba2f
Igor Canedo <igor.canedo@corp.globo.com>
2014-10-26 11:15:46 -0200

    melhoria no cursoextensao-historico_downloads.js

--- downloads/static/js/cursoextensao-historico_downloads.js
+++ downloads/static/js/cursoextensao-historico_downloads.js
@@ -1,12 +1,31 @@
-var resultList = document.getElementById('result_list'),
-    resultListBody = resultList.getElementsByTagName('tbody')[0],
-    lengthOfResultList = resultListBody.getElementsByTagName('tr').length,
-    paginator = document.getElementsByClassName('paginator')[0];
+var adminHistoricoDownload = adminHistoricoDownload || {};
 
-if (lengthOfResultList == 1){
-    paginator.textContent = lengthOfResultList + ' download realizado';
-} else if (lengthOfResultList == 0) {
-    paginator.textContent = 'Nenhum download realizado';
-} else {
-    paginator.textContent = lengthOfResultList + ' downloads realizados';
-}
+adminHistoricoDownload = (function () {
+    'use strict';
+
+    var resultList = document.getElementById('result_list'),
+        resultListBody = resultList.getElementsByTagName('tbody')[0],
+        lengthOfResultList = resultListBody.getElementsByTagName('tr').length,
+        paginator = document.getElementsByClassName('paginator')[0];
+
+    function init() {
+        mensagemHistoricoDownload();
+    }
+
+    function mensagemHistoricoDownload() {
+        if (lengthOfResultList == 1) {
+            paginator.textContent = lengthOfResultList + ' download realizado';
+        } else if (lengthOfResultList == 0) {
+            paginator.textContent = 'Nenhum download realizado';
+        } else {
+            paginator.textContent = lengthOfResultList + ' downloads realizados';
+        }
+    }
+
+    return {
+        init: init
+    };
+
+})();
+
+adminHistoricoDownload.init();
