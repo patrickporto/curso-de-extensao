0902324a53dd06e29e74c859ea1c083be32046ab
Igor Canedo <igor.canedo@corp.globo.com>
2014-10-25 22:42:03 -0200

    não existe mais filtragem por período logado como aluno em disciplinas

--- disciplina/views.py
+++ disciplina/views.py
@@ -2,22 +2,13 @@
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
-from django.contrib import messages
-from disciplina.models import Avaliacao, Periodo
+from disciplina.models import Avaliacao
 
 
 @user_passes_test(lambda u: u.tipo == u.ALUNO)
 def disciplinas(request):
-    periodo_id = int(request.GET.get('periodo', 0))
     context = {}
     avaliacoes = Avaliacao.objects.filter(aluno=request.user)
-    periodos = Periodo.objects.filter(pk__in=avaliacoes.values('disciplina__periodo').distinct())
-    if periodo_id:
-        avaliacoes = avaliacoes.filter(disciplina__periodo__id=periodo_id)
-    if avaliacoes.count() == 0 and periodo_id:
-        messages.add_message(request, messages.ERROR, 'O período informado não existe')
-
-    context['periodo_id'] = periodo_id
     context['avaliacoes'] = avaliacoes
-    context['periodos'] = periodos
+
     return render(request, 'disciplinas.html', context)
