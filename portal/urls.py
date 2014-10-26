f7a315bddb1023d2543d47cf0e1029d35d86f367
Igor Canedo <igor.canedo@corp.globo.com>
2014-10-25 22:42:56 -0200

    rota contato não existe mais

--- portal/urls.py
+++ portal/urls.py
@@ -2,11 +2,12 @@
 
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('portal.views',
+urlpatterns = patterns(
+    'portal.views',
     url(r'^$', 'home', name='home'),
-    url(r'^contato/$', 'contato', name='contato'),
     url(r'^access/$', 'access', name='access'),
     url(r'^logout/$', 'user_logout', name='logout'),
+    url(r'^witty/$', 'witty', name='witty'),
     url(r'^alterar_senha/$', 'alterar_senha', name='alterar_senha'),
     url(r'^disciplinas/', include('disciplina.urls')),
     url(r'^perfil/', include('pessoa.urls')),
