64e10a5f3c613c9d1a2573d8fd64a4ccc5b7bbfe
Patrick Porto <patrick.s.porto@gmail.com>
2014-11-21 15:50:46 -0200

    Atualização dos testes

--- disciplina/tests.py
+++ disciplina/tests.py
@@ -2,7 +2,7 @@
 
 from django.test import TestCase
 from pessoa.models import Pessoa
-from disciplina.models import Disciplina, Avaliacao, Periodo
+from disciplina.models import Disciplina, Avaliacao
 from datetime import date
 
 
@@ -18,18 +18,15 @@ class DisciplinaTest(TestCase):
         self.professor.save()
 
     def test_create_disciplina(self):
-        periodo = Periodo()
-        periodo.nome = '1 Semestre'
-        periodo.data_inicio = date(2014, 1, 19)
-        periodo.data_termino = date(2014, 5, 19)
-        periodo.save()
 
         disciplina = Disciplina()
         disciplina.nome = ''
         disciplina.limite_faltas = 10
+        disciplina.limite_abonos = 0
+        disciplina.data_inicio = date(2014, 1, 19)
+        disciplina.data_termino = date(2014, 5, 19)
         disciplina.professor = self.professor
         disciplina.save()
-        disciplina.periodo.add(periodo)
 
         aluno = Pessoa()
         aluno.nome = 'Fulaninho'
@@ -66,6 +63,7 @@ class AvaliacaoTest(TestCase):
         self.disciplina = Disciplina()
         self.disciplina.nome = 'Português'
         self.disciplina.limite_faltas = 10
+        self.disciplina.limite_abonos = 0
         self.disciplina.data_inicio = date(2014, 1, 19)
         self.disciplina.data_termino = date(2014, 5, 19)
         self.disciplina.professor = self.professor
@@ -88,6 +86,7 @@ class AvaliacaoTest(TestCase):
         disciplina_2 = Disciplina()
         disciplina_2.nome = 'Matemática'
         disciplina_2.limite_faltas = 10
+        disciplina_2.limite_abonos = 0
         disciplina_2.data_inicio = date(2014, 1, 19)
         disciplina_2.data_termino = date(2014, 5, 19)
         disciplina_2.professor = self.professor
