593dc6f8234c4f2c35ffe8a8aae219aee0bc5c25
Patrick Porto <patrick.s.porto@gmail.com>
2014-11-21 21:46:53 -0200

    Criação de teste para validar se as notas das disciplinas estão sendo zeradas

--- disciplina/tests.py
+++ disciplina/tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
+from django.forms.models import model_to_dict
 from pessoa.models import Pessoa
 from disciplina.models import Disciplina, Avaliacao
 from datetime import date
@@ -109,3 +110,66 @@ class AvaliacaoTest(TestCase):
         self.disciplina.save()
 
         self.assertEqual(Avaliacao.objects.count(), 0)
+
+
+class DisciplinaAdminTest(TestCase):
+    def setUp(self):
+        self.professor = Pessoa()
+        self.professor.nome = 'Fulano'
+        self.professor.sobrenome = 'da Silva'
+        self.professor.data_nascimento = date(1984, 1, 19)
+        self.professor.cpf = '555.555.555-55'
+        self.professor.tipo = Pessoa.PROFESSOR
+        self.professor.save()
+
+        self.aluno = Pessoa()
+        self.aluno.nome = 'Fulaninho'
+        self.aluno.sobrenome = 'de Souza'
+        self.aluno.data_nascimento = date(1994, 1, 19)
+        self.aluno.cpf = '111.111.111-11'
+        self.aluno.tipo = Pessoa.ALUNO
+        self.aluno.save()
+
+        self.disciplina = Disciplina()
+        self.disciplina.nome = 'Português'
+        self.disciplina.limite_faltas = 10
+        self.disciplina.limite_abonos = 0
+        self.disciplina.data_inicio = date(2014, 1, 19)
+        self.disciplina.data_termino = date(2014, 5, 19)
+        self.disciplina.professor = self.professor
+        self.disciplina.save()
+        self.disciplina.aluno.add(self.aluno)
+        self.disciplina.save()
+
+        funcionario = Pessoa()
+        funcionario.nome = 'Fulano',
+        funcionario.set_password('123')
+        funcionario.email = 'fulano@fulanocorp.com'
+        funcionario.sobrenome = 'da Silva'
+        funcionario.data_nascimento = date(1994, 1, 19)
+        funcionario.cpf = '000.000.000-00'
+        funcionario.tipo = Pessoa.FUNCIONARIO
+        funcionario.save()
+
+        self.c = Client()
+        self.c.login(cpf=funcionario.cpf, password='123')
+
+    def test_editar_disciplina_sem_apagar_avaliacoes(self):
+        avaliacao = Avaliacao.objects.get(disciplina=self.disciplina,
+                                          aluno=self.aluno)
+        avaliacao.faltas = 6
+        avaliacao.nota = 8
+        avaliacao.save()
+
+        data = model_to_dict(self.disciplina)
+        data['nome'] = 'Português 2'
+
+        response = self.c.post(
+            reverse('admin:disciplina_disciplina_change',
+                    args=(self.disciplina.id,)),
+            data
+        )
+        self.assertEqual(response.status_code, 302)
+
+        avaliacao_2 = Avaliacao.objects.first()
+        self.assertEqual(avaliacao_2.nota, avaliacao.nota)
